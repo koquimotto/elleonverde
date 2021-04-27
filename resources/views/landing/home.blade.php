8da0a51e5b9afaf84524739e84f5859028174f5c
Luis Paredes <luis.paredescaipo@gmail.com>
2021-04-26 21:03:43 -0500

    Nuevo inicio

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -103,6 +103,18 @@
                     <!--Gallery One Single-->
                     <div class="gallery_one_single">
                         <div class="gallery_one_image">
+                            <img src="http://img.youtube.com/vi/3qBeh-r3jz8/mqdefault.jpg">
+                            <div class="gallery_one_hover_box">
+                                <div class="gallery_one_icon">
+                                    <a href="https://www.youtube.com/watch?v=3qBeh-r3jz8" class="popup-video"><span
+                                            class="fa fa-play"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Gallery One Single-->
+                    <div class="gallery_one_single">
+                        <div class="gallery_one_image">
                             <img src="http://img.youtube.com/vi/fPslyTU7wnU/mqdefault.jpg">
                             <div class="gallery_one_hover_box">
                                 <div class="gallery_one_icon">
